9e8b31872bd29a173dc08cda6dfd784a7eaf5ea3
Wilco Schoneveld <schoneveld.wj@gmail.com>
2019-11-03 16:01:01 +0100

    Added react-three-fiber with demo code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import { Canvas, useFrame } from "react-three-fiber";
+
+function Thing() {
+  const ref = useRef();
+  useFrame(() => (ref.current.rotation.x = ref.current.rotation.y += 0.01));
+  return (
+    <mesh
+      ref={ref}
+      onClick={e => console.log("click")}
+      onPointerOver={e => console.log("hover")}
+      onPointerOut={e => console.log("unhover")}
+    >
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshNormalMaterial attach="material" />
+    </mesh>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas>
+      <Thing />
+    </Canvas>
   );
 }
 
