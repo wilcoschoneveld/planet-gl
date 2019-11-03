9d6ddbb7ed1db09a6f08fc9c23b737a191be0537
Wilco Schoneveld <schoneveld.wj@gmail.com>
2019-11-03 16:40:56 +0100

    Added orbit controls and a first planet

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React, { useRef } from "react";
-import { Canvas, useFrame } from "react-three-fiber";
+import { Canvas, extend, useFrame, useThree } from "react-three-fiber";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-function Thing() {
+extend({ OrbitControls });
+
+function Controls() {
+  const controls = useRef();
+  const { camera, gl } = useThree();
+  useFrame(() => controls.current.update());
+  return (
+    <orbitControls
+      ref={controls}
+      args={[camera, gl.domElement]}
+      enableDamping
+      dampingFactor={0.1}
+      rotateSpeed={0.5}
+    />
+  );
+}
+
+function SpaceScene() {
   const ref = useRef();
   useFrame(() => (ref.current.rotation.x = ref.current.rotation.y += 0.01));
+
   return (
-    <mesh
-      ref={ref}
-      onClick={e => console.log("click")}
-      onPointerOver={e => console.log("hover")}
-      onPointerOut={e => console.log("unhover")}
-    >
-      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-      <meshNormalMaterial attach="material" />
-    </mesh>
+    <scene>
+      <ambientLight args={[0xffffff, 0.4]} />
+      <directionalLight args={[0xffffff, 0.5]} position={[1, 1, 1]} />
+      <mesh ref={ref}>
+        <sphereBufferGeometry attach="geometry" args={[1, 36, 36]} />
+        <meshLambertMaterial attach="material" color={0x00ff00} />
+      </mesh>
+    </scene>
   );
 }
 
 function App() {
   return (
     <Canvas>
-      <Thing />
+      <Controls />
+      <SpaceScene />
     </Canvas>
   );
 }
